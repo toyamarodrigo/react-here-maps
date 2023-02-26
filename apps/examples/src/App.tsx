37be46c5def50e9876482839fa339ec34bcd90ef
rodrigo.toyama <rodrigo.toyama@urbetrack.com>
2023-02-26 03:33:51 -0300

    add HereDomMarker

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,53 +1,66 @@
-import { useState } from 'react'
-import { HereMapsProvider, HereMarker } from 'react-here-maps'
+import { useState } from "react";
+import { HereMapsProvider, HereMarker } from "react-here-maps";
 
-import { HERE_MAPS_APIKEY } from './constants'
+import { HERE_MAPS_APIKEY } from "./constants";
 
 function App() {
-  const [mapContainer, setMapContainer] = useState<HTMLDivElement | null>(null)  
+  const [mapContainer, setMapContainer] = useState<HTMLDivElement | null>(null);
+
+  const customIcon = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+  <rect stroke="white" fill="#1b468d" x="1" y="1" width="22" height="22" />
+  <text x="12" y="18" font-size="12pt" font-family="Arial" font-weight="bold" text-anchor="middle" fill="white">
+    H
+  </text>
+</svg>`;
 
   const positions = [
-    { lat: -34.603722, lng: -58.381592 },
-    { lat: -34.602722, lng: -58.381592 },
-    { lat: -34.601722, lng: -58.381592 },
-  ]
+    { lat: -34.603722, lng: -58.401592 },
+    { lat: -34.602722, lng: -58.411592, icon: customIcon },
+    { lat: -34.601722, lng: -58.421592 },
+  ];
 
   return (
-    <div style={{
-      width: "100vw",
-      height: "100vh",
-    }}>
-    <HereMapsProvider
-      apiKey={HERE_MAPS_APIKEY}
-      mapContainer={mapContainer}
-      mapOptions={{
-        center: {
-          lat: -34.603722,
-          lng: -58.381592,
-        },
-        zoom: 12,
-      }}
-      layerOptions={{
-        style: "normal",
-        ppi: 72,
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
       }}
     >
-      <div
-        ref={(node) => setMapContainer(node)}
-        style={{ width: "100%", height: "100vh" }}
-      />
-      {positions.map((position, index) => (
-        <HereMarker 
-          key={index}
-          positions={position}
-          data={{
-            title: `Marker ${index}`,
-          }}
+      <HereMapsProvider
+        apiKey={HERE_MAPS_APIKEY}
+        mapContainer={mapContainer}
+        mapOptions={{
+          center: {
+            lat: -34.603722,
+            lng: -58.381592,
+          },
+          zoom: 12,
+        }}
+        layerOptions={{
+          style: "normal",
+          ppi: 320,
+        }}
+      >
+        <div
+          ref={(node) => setMapContainer(node)}
+          style={{ width: "100%", height: "100vh" }}
+        />
+        {positions.map((position, index) => (
+          <HereMarker
+            key={index}
+            positions={position}
+            data={{
+              title: `Marker ${index}`,
+            }}
+            icon={position.icon || undefined}
+            iconOptions={{
+              stickHeight: 80,
+            }}
           />
-      ))}
-    </HereMapsProvider>
+        ))}
+      </HereMapsProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ packages/lib/src/components/HereDomMarker.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useHereMaps } from "../hooks/useHereMaps";
+
+interface HereMarkerProps extends Omit<H.map.Marker.Options, "icon"> {
+  children?: React.ReactNode;
+  icon?: string;
+  iconOptions?: H.map.Icon.Options;
+  positions: H.geo.IPoint;
+}
+
+/**
+ * A marker which supports HTML (and SVG) content, which can be dynamic. 
+ * Markers of this type are best displayed individually or in small sets.
+ * @param props 
+ * @returns 
+ */
+export const HereMarker = (props: HereMarkerProps) => {
+  const map = useHereMaps();
+  const { positions, icon, iconOptions, ...options } = props;
+  const [marker, setMarker] = useState<H.map.Marker | null>(null);
+
+  const createDomMarker = () => {
+    let iconMarker: H.map.Icon | undefined;
+    if (icon) {
+      iconMarker = new H.map.Icon(icon, iconOptions);
+    }
+
+    const marker = new H.map.DomMarker(positions, {
+      icon: iconMarker,
+      ...options,
+    });
+
+    setMarker(marker);
+  };
+
+  useEffect(() => {
+    if (map && !marker) createDomMarker();
+  }, [map, marker]);
+
+  useEffect(() => {
+    if (map && marker) map.addObject(marker);
+  }, [map, marker]);
+
+  return null;
+};
