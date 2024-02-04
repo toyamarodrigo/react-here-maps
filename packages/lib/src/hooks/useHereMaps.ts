89967b94f7310f3732c1735b0f92dd6eca7988c8
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2024-02-04 20:53:04 -0300

    Refactor useHereMaps hook to use MutableRefObject

--- packages/lib/src/hooks/useHereMaps.ts
+++ packages/lib/src/hooks/useHereMaps.ts
@@ -1,26 +1,27 @@
 import type H from "@here/maps-api-for-javascript";
+import type { MutableRefObject } from "react";
 import { useContext } from "react";
-import { HereMapsContext } from "../components/HereMap/HereMap.provider";
+import { MapContext } from "../components/HereMap/HereMap.provider";
 
-interface UseHereMapsProps {
-  map: H.Map;
-  platform: H.service.Platform;
+interface UseHereMapProps {
+  map: MutableRefObject<H.Map | null>;
+  platform: MutableRefObject<H.service.Platform | null>;
 }
 
 /**
  * @name useHereMaps
  * @description
  * This hook is used to get the map instance.
- * @returns {UseHereMapsProps} - Map instance
+ * @returns {UseHereMapProps} - Map instance
  * @example
- * const { map } = useHereMaps();
+ * const { map, platform } = useHereMaps();
  */
 
-export const useHereMaps = (): UseHereMapsProps => {
-  const context = useContext(HereMapsContext);
+export const useHereMaps = (): UseHereMapProps => {
+  const context = useContext(MapContext);
 
-  if (context === undefined) {
-    throw new Error("useHereMaps must be used within a HereMapsProvider");
+  if (!context) {
+    throw new Error("useHereMaps must be used within a MapProvider");
   }
 
   return context;
